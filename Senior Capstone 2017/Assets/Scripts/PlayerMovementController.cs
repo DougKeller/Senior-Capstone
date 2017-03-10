ac15fe784e5ad1317edd0c07ab7068cac7a086f2
Doug Keller <dkeller@synergydatasystems.com>
2017-03-10 02:55:47 -0500

    Add snake animations

---
+++ Senior Capstone 2017/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+	Rigidbody2D rigidBody;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
+		Vector2 movement = new Vector2 (horizontal, vertical);
+		movement.Normalize ();
+
+		if (movement != Vector2.zero) {
+			animator.SetBool ("isWalking", true);
+			animator.SetFloat ("inputX", movement.x);
+			animator.SetFloat ("inputY", movement.y);
+		} else {
+			animator.SetBool ("isWalking", false);
+		}
+
+		Vector2 position = rigidBody.position + movement * Time.deltaTime;
+		rigidBody.MovePosition (position);
+	}
+}
