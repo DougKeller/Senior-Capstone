49af95c902624502926317603277772c56c632e0
DougKeller <dwk24@zips.uakron.edu>
2017-04-23 01:09:49 -0400

    Add an experience bar

--- Senior Capstone 2017/Assets/Scripts/GUI/Bar.cs
+++ Senior Capstone 2017/Assets/Scripts/GUI/Bar.cs
@@ -6,55 +6,56 @@ namespace GUI
 {
   class Bar : MonoBehaviour
   {
-		public GameObject target;
+    public GameObject target;
 
-		public Image healthBar;
-		public Image staminaBar;
-		public Image healthBackBar;
-		public Image staminaBackBar;
+    public Image healthBar;
+    public Image staminaBar;
+    public Image experienceBar;
+    public Image healthBackBar;
+    public Image staminaBackBar;
+    public Image experienceBackBar;
 
     float HealthAmount ()
     {
       if (!target) return 0f;
-			Statistics.Stats stats = target.GetComponent<Statistics.Stats> ();
-			return stats.hitpoints / (float)stats.MaxHitpoints ();
+      Statistics.Stats stats = target.GetComponent<Statistics.Stats> ();
+      return stats.hitpoints / (float)stats.MaxHitpoints ();
     }
 
     float StaminaAmount ()
     {
       if (!target) return 0f;
       Statistics.Stats stats = target.GetComponent<Statistics.Stats> ();
-//			return stats.stamina / 100f;
-			return stats.PercentOfLevelComplete ();
+      return stats.stamina / 100f;
+    }
+
+    float ExperienceAmount ()
+    {
+      if (!target) return 0f;
+      Statistics.Stats stats = target.GetComponent<Statistics.Stats> ();
+      return stats.PercentOfLevelComplete ();
+    }
+
+    void updateBar(Image backBar, Image frontBar, float amount)
+    {
+      float delta1 = (frontBar.fillAmount - amount) * Time.deltaTime;
+      float delta2 = (backBar.fillAmount - amount) * Time.deltaTime;
+      if (delta1 < 0) {
+        delta1 *= 2f;
+        delta2 *= 5f;
+      } else {
+        delta1 *= 5f;
+        delta2 *= 2f;
+      }
+      frontBar.fillAmount -= delta1;
+      backBar.fillAmount -= delta2;
     }
 
     void Update ()
-		{
-			float healthAmount = HealthAmount ();
-			float healthDelta1 = (healthBar.fillAmount - healthAmount) * Time.deltaTime;
-			float healthDelta2 = (healthBackBar.fillAmount - healthAmount) * Time.deltaTime;
-			if (healthDelta1 < 0) {
-				healthDelta1 *= 2f;
-				healthDelta2 *= 5f;
-			} else {
-				healthDelta1 *= 5f;
-				healthDelta2 *= 2f;
-			}
-			healthBar.fillAmount -= healthDelta1;
-			healthBackBar.fillAmount -= healthDelta2;
-
-			float staminaAmount = StaminaAmount ();
-			float staminaDelta1 = (staminaBar.fillAmount - staminaAmount) * Time.deltaTime;
-			float staminaDelta2 = (staminaBackBar.fillAmount - staminaAmount) * Time.deltaTime;
-			if (staminaDelta1 < 0) {
-				staminaDelta1 *= 2f;
-				staminaDelta2 *= 5f;
-			} else {
-				staminaDelta1 *= 5f;
-				staminaDelta2 *= 2f;
-			}
-			staminaBar.fillAmount -= staminaDelta1;
-			staminaBackBar.fillAmount -= staminaDelta2;
+    {
+      updateBar(healthBackBar, healthBar, HealthAmount ());
+      updateBar(staminaBackBar, staminaBar, StaminaAmount ());
+      updateBar(experienceBackBar, experienceBar, ExperienceAmount ());
     }
   }
 }
